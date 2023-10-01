eb7692b8cd5a19516a782f0e64b7d6113cab273f
Isadora Malohlava <malohlava,isadora@gmail.com>
2023-10-01 11:38:07 -0300

    arrumando CSS e JS registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,6 +6,36 @@ import { BsApple } from 'react-icons/bs';
 import { Photo } from '../components/Photo';
 
 export function Register() {
+    function validateEmail(email) {
+        if (!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)) {
+            const err = new Error('Email invalido.')
+            err.input = 'email'
+            throw err
+        }
+    }
+
+    const userInputs = {
+        name: document.querySelector('#name'),
+        email: document.querySelector('#email'),
+    }
+
+    const form = document.querySelector('form')
+
+    form.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+
+        try {
+            userInputs.name.classList.add('sucess')
+            validateEmail(userInputs.email.value)
+            userInputs.email.classList.add('sucess')
+        } catch (err) {
+            userInputs[err.input].classList.add('error')
+            document.querySelector(`#${err.input}-error`).textContent = err.message
+        }
+    })
+
+
+
     return (
         <div id={styles.WelcomeDiv}>
             <div>
@@ -41,17 +71,21 @@ export function Register() {
                     
                     </div>
                     <div className={styles.registerInputs}>
-                        <p className={styles.name}>Primeiro Nome</p>
-                        <input type="name" name="" id="" placeholder="Coloque seu Primeiro Nome" className={styles.wrapperInputs}/>
+                        <form action="">
+                            <p className={styles.name}>Primeiro Nome</p>
+                            <input type="name" name="" id={styles.name} placeholder="Coloque seu Primeiro Nome" className={styles.wrapperInputs}/>
+                            <span id={styles.name-error}></span>
 
-                        <p className={styles.name}>Sobrenome</p>
-                        <input type="name" name="" id="" placeholder="Coloque seu Sobrenome" className={styles.wrapperInputs}/>
+                            <p className={styles.name}>Sobrenome</p>
+                            <input type="name" name="" id={styles.surName} placeholder="Coloque seu Sobrenome" className={styles.wrapperInputs}/>
+                            <span id={styles.surName-error}></span>
 
-                        <p className={styles.name}>Email</p>
-                        <input type="email" name="" id="" placeholder="Coloque seu Email" className={styles.wrapperInputs}/>
+                            <p className={styles.name}>Email</p>
+                            <input type="email" name="" id={styles.email} placeholder="Coloque seu Email" className={styles.wrapperInputs}/>
+                            <span id={styles.email-error}></span>
 
-                        <Link to="/password"><input type="button" value="Continue" id={styles.buttonContinue}/> </Link>
-
+                            <Link to="/password"><input type="button" value="Continue" id={styles.buttonContinue}/> </Link>
+                        </form>
                     </div>
 
                         <div id={styles.divProgress}>
@@ -63,6 +97,6 @@ export function Register() {
                 <div className={styles.photo}>
                     <Photo/>
                 </div>
-        </div>
-    )
+        </div>
+    )
 }
